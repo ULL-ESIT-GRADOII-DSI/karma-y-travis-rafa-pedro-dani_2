71cf897e39c84ca58e764be12e71e8e7a435fa64
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-03-05 19:00:06 +0000

    Terminados grados Celsius

--- assets/js/celsius.js
+++ assets/js/celsius.js
@@ -5,12 +5,14 @@
 
   function Celsius(valor)
   {
+    this.valor = valor;
+    this.tipo = "C";
   }
   exports.Celsius = Celsius;
   Celsius.prototype.toFahrenheit = function() {
-    return 0;
+    return (this.valor *9/5) + 32;
   };
   Celsius.prototype.toKelvin = function() {
-    return 0;
+    return (this.valor +273.15);
   };
 })(this);
