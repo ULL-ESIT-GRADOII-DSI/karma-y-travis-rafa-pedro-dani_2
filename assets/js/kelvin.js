f593828234a70e4b8d0c3fa0afa83b8d84b7818a
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-06 15:43:14 +0000

    Grados Kelvin terminado

--- assets/js/kelvin.js
+++ assets/js/kelvin.js
@@ -5,12 +5,14 @@
 
   function Kelvin(valor)
   {
+    this.valor=valor;
+    this.tipo='k';
   }
   exports.Kelvin = Kelvin;
   Kelvin.prototype.toCelsius = function() {
-    return 0;
+    return (this.valor-273.15);
   };
   Kelvin.prototype.toFahrenheit = function() {
-    return 0;
+    return (this.valor-273.15)*(9/5)+32;
   };
 })(this);
