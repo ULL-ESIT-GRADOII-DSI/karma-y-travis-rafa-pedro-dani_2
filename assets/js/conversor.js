3485053f567e65a39d46bd7d541fbb4623e9356c
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-07 19:13:17 +0000

    Cambiado el switch y añadidas  condiciones necesarias

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -19,17 +19,18 @@
   exports.Temperatura = Temperatura;
 
   exports.convertir = function() {
-    var elemento_cel  = document.getElementById('converted_cel'),
-        elemento_far  = document.getElementById('converted_far'),
-        elemento_kel  = document.getElementById('converted_kel'),
-        elemento_met  = document.getElementById('converted_met'),
-        elemento_pul  = document.getElementById('converted_pul'),
+      var elemento_cel  = document.getElementById('converted_cel'),
+          elemento_far  = document.getElementById('converted_far'),
+          elemento_kel  = document.getElementById('converted_kel'),
+          elemento_met  = document.getElementById('converted_met'),
+          elemento_pul  = document.getElementById('converted_pul');
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         var regexp=XRegExp('(?<entrada>^[ ]*([-+]?[0-9]+(?:\.[0-9]+)?[ ]*(?:e[+-]?[1-9]+)?))#entrada \n\
         (?<medida>[ ]*([cCfFkKmM])?) #medida \n\
         (?<parteopcional> [ ]*([tT][o])?[ ]*) #parteopcional \n\
          (?<conversion>[ ]*[cCfFkKmMpP][ ]*) #conversion','x');
          var valor=XRegExp.exec(document.getElementById('convert').value,regexp);
+         console.log(valor.parteopcional);
     if (valor) {
       var numero = valor.entrada,
           tipomedida=(valor.medida).toLowerCase(),
@@ -39,36 +40,138 @@
           console.log("tipoconversion: "+tipoconversion);
 
       numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
 
-      switch (tipo) {
+
+      if(tipomedida==="")
+      {
+      switch (tipoconversion) {
         case 'c':
-          var celsius = new Celsius(numero);
-          elemento_cel.innerHTML = numero.toFixed(2) + " Celsius";
-          elemento_far.innerHTML = celsius.toFahrenheit().toFixed(2) + " Fahrenheit";
-          elemento_kel.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
+            var celsius = new Celsius(numero);
+            elemento_cel.innerHTML = numero.toFixed(2) + " Celsius";
+            elemento_far.innerHTML = celsius.toFahrenheit().toFixed(2) + " Fahrenheit";
+            elemento_kel.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
           break;
         case 'f':
-          var fahrenheit = new Fahrenheit(numero);
-          elemento_cel.innerHTML = fahrenheit.toCelsius().toFixed(2) + " Celsius";
-          elemento_far.innerHTML = numero.toFixed(2) + " Fahrenheit";
-          elemento_kel.innerHTML = fahrenheit.toKelvin().toFixed(2) + " Kelvin";
+            var fahrenheit = new Fahrenheit(numero);
+            elemento_cel.innerHTML = fahrenheit.toCelsius().toFixed(2) + " Celsius";
+            elemento_far.innerHTML = numero.toFixed(2) + " Fahrenheit";
+            elemento_kel.innerHTML = fahrenheit.toKelvin().toFixed(2) + " Kelvin";
           break;
         case 'k':
-          var kelvin = new Kelvin(numero);
-          elemento_cel.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
-          elemento_far.innerHTML = kelvin.toFahrenheit().toFixed(2) + " Fahrenheit";
-          elemento_kel.innerHTML = numero.toFixed(2) + " Kelvin";
+            var kelvin = new Kelvin(numero);
+            elemento_cel.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
+            elemento_far.innerHTML = kelvin.toFahrenheit().toFixed(2) + " Fahrenheit";
+            elemento_kel.innerHTML = numero.toFixed(2) + " Kelvin";
           break;
         case 'm':
-          var metro = new Metro(numero);
-          elemento_pul.innerHTML = metro.toPulgada().toFixed(2) + " Pulgadas";
-          elemento_met.innerHTML = numero.toFixed(2) + " Metros";
+            var metro = new Metro(numero);
+            elemento_pul.innerHTML = metro.toPulgada().toFixed(2) + " Pulgadas";
+            elemento_met.innerHTML = numero.toFixed(2) + " Metros";
           break;
         case 'p':
-          var pulgada = new Pulgada(numero);
-          elemento_met.innerHTML = pulgada.toMetro().toFixed(2) + " Metros";
-          elemento_pul.innerHTML = numero.toFixed(2) + " Pulgadas";
+            var pulgada = new Pulgada(numero);
+            elemento_met.innerHTML = pulgada.toMetro().toFixed(2) + " Metros";
+            elemento_pul.innerHTML = numero.toFixed(2) + " Pulgadas";
+          break;
+        default:
+          elemento_cel.innerHTML = "";
+          elemento_far.innerHTML = "";
+          elemento_kel.innerHTML = "";
+          elemento_met.innerHTML = "";
+          elemento_pul.innerHTML = "";
+      }
+
+  }
+  else {
+      switch (tipoconversion) {
+        case 'c':
+
+          if(tipomedida=='c')
+          {
+
+            var celsius = new Celsius(numero);
+            elemento_cel.innerHTML = numero.toFixed(2) + " Celsius";
+          }
+          else if(tipomedida=='f')
+          {
+
+            var fahrenheit = new Fahrenheit(numero);
+            elemento_cel.innerHTML = fahrenheit.toCelsius().toFixed(2) + " Celsius";
+          }
+          else if(tipomedida=='k')
+          {
+
+            var kelvin = new Kelvin(numero);
+              elemento_cel.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
+          }
+        break;
+
+
+
+        case 'f':
+          if(tipomedida=='c')
+          {
+            var celsius = new Celsius(numero);
+            elemento_far.innerHTML = celsius.toFahrenheit().toFixed(2) + " Fahrenheit";
+          }
+          else if(tipomedida=='f')
+          {
+            var fahrenheit = new Fahrenheit(numero);
+            elemento_far.innerHTML = numero.toFixed(2) + " Fahrenheit";
+          }
+          else if(tipomedida=='k')
+          {
+            var kelvin = new Kelvin(numero);
+          elemento_far.innerHTML = kelvin.toFahrenheit().toFixed(2) + " Fahrenheit";
+          }
+        break;
+
+
+
+
+        case 'k':
+          if(tipomedida=='c')
+          {
+            var celsius = new Celsius(numero);
+            elemento_kel.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
+          }
+          else if(tipomedida=='f')
+          {
+            var fahrenheit = new Fahrenheit(numero);
+            elemento_kel.innerHTML = fahrenheit.toKelvin().toFixed(2) + " Kelvin";
+          }
+          else if(tipomedida=='k')
+          {
+            var kelvin = new Kelvin(numero);
+            elemento_kel.innerHTML = numero.toFixed(2) + " Kelvin";
+          }
+        break;
+
+        case 'm':
+          if(tipomedida=='m')
+          {
+            var metro = new Metro(numero);
+            elemento_met.innerHTML = numero.toFixed(2) + " Metros";
+          }
+          else if(tipomedida=='p')
+          {
+            var pulgada = new Pulgada(numero);
+            elemento_met.innerHTML = pulgada.toMetro().toFixed(2) + " Metros";
+          }
+          break;
+
+        case 'p':
+          if(tipomedida=='m')
+          {
+            var metro = new Metro(numero);
+            elemento_pul.innerHTML = metro.toPulgada().toFixed(2) + " Pulgadas";
+
+          }
+          else if(tipomedida=='p')
+          {
+            var pulgada = new Pulgada(numero);
+            elemento_pul.innerHTML = numero.toFixed(2) + " Pulgadas";
+          }
           break;
         default:
           elemento_cel.innerHTML = "";
@@ -78,12 +181,13 @@
           elemento_pul.innerHTML = "";
       }
     }
-    else{
-      elemento_cel.innerHTML = "";
-      elemento_far.innerHTML = "";
-      elemento_kel.innerHTML = "";
-      elemento_met.innerHTML = "";
-      elemento_pul.innerHTML = "";
-    }
   }
+    else{
+      elemento_cel.innerHTML = "error";
+      elemento_far.innerHTML = "error";
+      elemento_kel.innerHTML = "error";
+      elemento_met.innerHTML = "error";
+      elemento_pul.innerHTML = "error";
+    }
+  };
 })(this);
