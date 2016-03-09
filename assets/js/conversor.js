47b25a7475f72e95c99350e5f4e9680574ee123c
Daniel Ramos <danielramosacosta@hotmail.com>
2016-03-09 10:32:13 +0000

    Pasado el lint a conversor.js y fahrenheit.js

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,103 +1,105 @@
-(function(exports) {
-  "use strict";
+(function (exports) {
+    "use strict";
 
-  function Medida(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
-    /* ademas de new Medida(45.2, "Km") */
-    this.valor = valor;
-    this.tipo = tipo;
-  }
-
-  Temperatura.prototype = new Medida;
-
-  function Temperatura(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
-    Medida.call(this,valor,tipo);
-  }
-
-  exports.Temperatura = Temperatura;
-
-  exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp = XRegExp('^\\s*  \n' +
-                         '(?<cantidad>  [+-]?[0-9]*\\.?[0-9]+([eE][+-]?[0-9]+)?)  # la cantidad, es numerica \n' +
-                         '\\s*   \n' +
-                         '(?<medida> (' +
-                                      '(f|fa|fah|fahr|fahre|fahren|fahrenh|fahrenhe|fahrenhei|fahrenheit)| # Posibles valores para fahrenheit \n' +
-                                      '(c|ce|cel|cels|celsi|celsiu|celsius)| # Valores de grados celsius \n' +
-                                      '(k|ke|kel|kelv|kelvi|kelvin)|       #grados kelvin\n' +
-                                      '(m|me|met|metr|metro|metros)| #metros \n' +
-                                      '(p|pu|pul|pulg|pulga|pulgad|pulgada|pulgadas) #pulgadas \n' +
-                                    ')) \n' +
-                         '\\s*   \n' +
-                         '(?<verbo> to)? \n' +
-                         '\\s*   \n' +
-                         '(?<medida_dest> (' +
-                                      '(f|fa|fah|fahr|fahre|fahren|fahrenh|fahrenhe|fahrenhei|fahrenheit)| # Posibles valores para fahrenheit \n' +
-                                      '(c|ce|cel|cels|celsi|celsiu|celsius)| # Valores de grados celsius \n' +
-                                      '(k|ke|kel|kelv|kelvi|kelvin)|       #grados kelvin\n' +
-                                      '(m|me|met|metr|metro|metros)| #metros \n' +
-                                      '(p|pu|pul|pulg|pulga|pulgad|pulgada|pulgadas) #pulgadas \n' +
-                                    ')) \n' +
-                         '\\s*   $', 'x');
-    valor = valor.toLowerCase();
-    valor = XRegExp.exec(valor,regexp);
-    if(valor){
-      var numero    = valor.cantidad,
-          tipo      = valor.medida[0], //Primer carácter del tipo, o sea f para fahrenheit, k para kelvin y c para celsius
-          tipo_dest = null;
-
-      if(valor.medida_dest)
-        tipo_dest = valor.medida_dest[0];
-
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo + ", Destino: " + tipo_dest);
-
-      var obj_numer = null;
-      switch (tipo) {
-        case 'c':
-          obj_numer = new Celsius(numero);
-          break;
-        case 'f':
-          obj_numer = new Fahrenheit(numero);
-          break;
-        case 'k':
-          obj_numer = new Kelvin(numero);
-          break;
-        case 'm':
-          obj_numer = new Metro(numero);
-          break;
-        case 'p':
-          obj_numer = new Pulgada(numero);
-          break;
-      }
-
-      switch (tipo_dest) {
-        case 'c':
-          elemento.innerHTML = obj_numer.toCelsius().toFixed(2) + " Celsius";
-          break;
-        case 'f':
-          elemento.innerHTML = obj_numer.toFahrenheit().toFixed(2) + " Fahrenheit";
-          break;
-        case 'k':
-          elemento.innerHTML = obj_numer.toKelvin().toFixed(2) + " Kelvin";
-          break;
-        case 'm':
-          elemento.innerHTML = obj_numer.toMetro().toFixed(2) + " Metros";
-          break;
-        case 'p':
-          elemento.innerHTML = obj_numer.toPulgada().toFixed(2) + " Pulgadas";
-          break;
-      }
+    function Medida(valor, tipo) {
+        /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
+        /* ademas de new Medida(45.2, "Km") */
+        this.valor = valor;
+        this.tipo = tipo;
     }
-    else{
-      console.error('Error en la expresión');
-      elemento.innerHTML = "Error en la entrada";
+
+    function Temperatura(valor, tipo) {
+        /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+        Medida.call(this, valor, tipo);
     }
-  };
+
+    Temperatura.prototype = new Medida();
+
+    exports.Temperatura = Temperatura;
+
+    exports.convertir = function () {
+        var valor = document.getElementById('convert').value,
+            elemento = document.getElementById('converted'),
+
+            /* Extienda la RegeExp a la especificación. use una XRegExp */
+            regexp = new XRegExp('^\\s* \n' +
+                           '(?<cantidad>  [+-]?[0-9]*\\.?[0-9]+([eE][+-]?[0-9]+)?)  # la cantidad, es numerica \n' +
+                           '\\s*   \n' +
+                           '(?<medida> (' +
+                               '(f|fa|fah|fahr|fahre|fahren|fahrenh|fahrenhe|fahrenhei|fahrenheit)| # Posibles valores para fahrenheit \n' +
+                               '(c|ce|cel|cels|celsi|celsiu|celsius)| # Valores de grados celsius \n' +
+                               '(k|ke|kel|kelv|kelvi|kelvin)|       #grados kelvin\n' +
+                               '(m|me|met|metr|metro|metros)| #metros \n' +
+                               '(p|pu|pul|pulg|pulga|pulgad|pulgada|pulgadas) #pulgadas \n' +
+                           ')) \n' +
+                           '\\s*   \n' +
+                           '(?<verbo> to)? \n' +
+                           '\\s*   \n' +
+                           '(?<medida_dest> (' +
+                               '(f|fa|fah|fahr|fahre|fahren|fahrenh|fahrenhe|fahrenhei|fahrenheit)| # Posibles valores para fahrenheit \n' +
+                               '(c|ce|cel|cels|celsi|celsiu|celsius)| # Valores de grados celsius \n' +
+                               '(k|ke|kel|kelv|kelvi|kelvin)|       #grados kelvin\n' +
+                               '(m|me|met|metr|metro|metros)| #metros \n' +
+                               '(p|pu|pul|pulg|pulga|pulgad|pulgada|pulgadas) #pulgadas \n' +
+                           ')) \n' +
+                           '\\s*   $', 'x');
+        valor = valor.toLowerCase();
+        valor = XRegExp.exec(valor, regexp);
+        if (valor) {
+            var numero = valor.cantidad,
+                tipo = valor.medida[0], //Primer carácter del tipo, o sea f para fahrenheit, k para kelvin y c para celsius
+                tipo_dest = valor.medida_dest[0];
+
+            numero = parseFloat(numero);
+            console.log("Valor: " + numero + ", Tipo: " + tipo + ", Destino: " + tipo_dest);
+
+            var obj_numer = null;
+            switch (tipo) {
+            case 'c':
+                obj_numer = new Celsius(numero);
+                break;
+            case 'f':
+                obj_numer = new Fahrenheit(numero);
+                break;
+            case 'k':
+                obj_numer = new Kelvin(numero);
+                break;
+            case 'm':
+                obj_numer = new Metro(numero);
+                break;
+            case 'p':
+                obj_numer = new Pulgada(numero);
+                break;
+            }
+            try {
+                switch (tipo_dest) {
+                case 'c':
+                    elemento.innerHTML = obj_numer.toCelsius().toFixed(2) + " Celsius";
+                    break;
+                case 'f':
+                    elemento.innerHTML = obj_numer.toFahrenheit().toFixed(2) + " Fahrenheit";
+                    break;
+                case 'k':
+                    elemento.innerHTML = obj_numer.toKelvin().toFixed(2) + " Kelvin";
+                    break;
+                case 'm':
+                    elemento.innerHTML = obj_numer.toMetro().toFixed(2) + " Metros";
+                    break;
+                case 'p':
+                    elemento.innerHTML = obj_numer.toPulgada().toFixed(2) + " Pulgadas";
+                    break;
+                }
+            } catch (err) {
+                if (tipo === tipo_dest) {
+                    elemento.innerHTML = numero.toFixed(2) + " " + obj_numer.constructor.name;
+                } else {
+                    console.error('No se convertir a ese tipo');
+                    elemento.innerHTML = "No se convertir de " + tipo + " a " + tipo_dest;
+                }
+            }
+        } else {
+            console.error('Error en la expresión');
+            elemento.innerHTML = "Error en la entrada";
+        }
+    };
 })(this);
--- assets/js/fahrenheit.js
+++ assets/js/fahrenheit.js
@@ -1,21 +1,20 @@
 /* Parte de Daniel */
 
-(function(exports) {
-  "use strict";
+(function (exports) {
+    "use strict";
 
-  function Fahrenheit(valor)
-  {
-    Temperatura.call(this,valor,"F");
-  }
+    function Fahrenheit(valor) {
+        Temperatura.call(this, valor, "F");
+    }
 
-  Fahrenheit.prototype = new Temperatura;
-  Fahrenheit.prototype.constructor = Fahrenheit;
+    Fahrenheit.prototype = new Temperatura();
+    Fahrenheit.prototype.constructor = Fahrenheit;
 
-  exports.Fahrenheit = Fahrenheit;
-  Fahrenheit.prototype.toCelsius = function() {
-    return (this.valor -32) * 5/9;
-  };
-  Fahrenheit.prototype.toKelvin = function() {
-    return (this.valor + 459.67) * 5/9;
-  };
+    exports.Fahrenheit = Fahrenheit;
+    Fahrenheit.prototype.toCelsius = function () {
+        return (this.valor - 32) * 5 / 9;
+    };
+    Fahrenheit.prototype.toKelvin = function () {
+        return (this.valor + 459.67) * 5 / 9;
+    };
 })(this);
