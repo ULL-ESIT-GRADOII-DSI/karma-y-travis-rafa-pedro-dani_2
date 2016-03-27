050bbdb9dcd5ea2401aa2694623b9401ef664c1e
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-27 20:47:47 +0100

    Prueba de herencia de Temperatura e iniciando Celsius

--- test/test_temperatura.js
+++ test/test_temperatura.js
@@ -1,5 +1,24 @@
 var expect = chai.expect;
 
-describe("Prueba de la herencia",function(){
-
+describe("Pruebas de Temperatura y herencia",function(){
+    describe("Probando herencia",function(){
+      it ("Temperatura hereda de Medida",function(){
+         function Temperatura(valor,tipo){
+             Medida.call(this, valor, tipo);
+         }
+        var t1=new Medida(2,"f");
+        t1.prototype instanceof Medida;
+        t1 instanceof Temperatura;
+        //t1.prototype instanceof Medida;
+      });
+      it ("Celsius hereda de Temperatura.",function(){
+        function Celsius(valor) {
+            Temperatura.call(this, valor, "C");
+            Celsius.prototype = new Temperatura();
+            Celsius.prototype.constructor = Celsius;
+        }
+        
+        //Celsius.prototype instanceof Temperatura;
+      });
+    });
 });
