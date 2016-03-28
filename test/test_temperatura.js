35a391f5d2ee52b59833b96fed40ef606ba02d98
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-28 21:39:02 +0200

    Creadas las pruebas para las diferentes temperaturas

--- test/test_temperatura.js
+++ test/test_temperatura.js
@@ -16,4 +16,42 @@ describe("Pruebas de Temperatura y herencia",function(){
         expect(Medida.measures.f.prototype).to.be.instanceof(Medida.measures.temperatura);
       });
     });
+    describe("Probando metodos Celsius",function(){
+      describe("Probando celsius",function(){
+        celsius = new Medida.measures.c(2);
+    
+        it ("Se debe poder convertir a Fahrenheit",function(){
+          expect(celsius.toFahrenheit().valor).to.equal(new Medida.measures.f(35.6).valor);
+        });
+        it ("Se debe poder convertir a Kelvin",function(){
+          expect(celsius.toKelvin().valor).to.equal(new Medida.measures.k(275.15).valor);
+        });
+
+      });
+    });
+    describe("Probando metodos Fahrenheit",function(){
+      describe("Probando Fahrenheit",function(){
+        fahrenheit = new Medida.measures.f(2);
+
+        it ("Se debe poder convertir a Celsius",function(){
+          expect(fahrenheit.toCelsius().valor).to.equal(new Medida.measures.c(-16.666666666666668).valor);
+        });
+        it ("Se debe poder convertir a Kelvin",function(){
+          expect(fahrenheit.toKelvin().valor).to.equal(new Medida.measures.k(256.48333333333335).valor);
+        });
+
+      });
+    });
+    describe("Probando metodos Kelvin",function(){
+      describe("Probando Kelvin",function(){
+        kelvin = new Medida.measures.k(2);
+        it ("Se debe poder convertir a Celsius",function(){
+          expect(kelvin.toCelsius().valor).to.equal(new Medida.measures.c(-271.15).valor);
+        });
+        it ("Se debe poder convertir a Fahrenheit",function(){
+          expect(kelvin.toFahrenheit().valor).to.equal(new Medida.measures.f(-456.07).valor);
+        });
+
+      });
+    });
 });
