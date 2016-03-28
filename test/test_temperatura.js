edd6fc804798eb1260f11d7866166b513ab02983
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-28 21:11:19 +0200

    Cambiadas pruebas de herencia celsius,fahrenheit,kelvin

--- test/test_temperatura.js
+++ test/test_temperatura.js
@@ -6,14 +6,14 @@ describe("Pruebas de Temperatura y herencia",function(){
           //console.log(Medida.measures.temperatura);
           expect(Medida.measures.temperatura.prototype).to.be.instanceof(Medida);
       });
-      it("Celsius hereda de Medida",function(){
-        expect(Medida.measures.c.prototype).to.be.instanceof(Medida);
+      it("Celsius hereda de Temperatura",function(){
+        expect(Medida.measures.c.prototype).to.be.instanceof(Medida.measures.temperatura);
       });
-      it("Kelvin hereda de Medida",function(){
-        expect(Medida.measures.k.prototype).to.be.instanceof(Medida);
+      it("Kelvin hereda de Temperatura",function(){
+        expect(Medida.measures.k.prototype).to.be.instanceof(Medida.measures.temperatura);
       });
-      it("Fahrenheit hereda de Medida",function(){
-        expect(Medida.measures.f.prototype).to.be.instanceof(Medida);
+      it("Fahrenheit hereda de Temperatura",function(){
+        expect(Medida.measures.f.prototype).to.be.instanceof(Medida.measures.temperatura);
       });
     });
 });
