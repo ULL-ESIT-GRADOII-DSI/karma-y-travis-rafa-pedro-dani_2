1ec7d50f483e7aa433a9519de935c19b04faa421
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-28 15:13:32 +0200

    Modificado test herencia e incluida herencias celcius,kelvin y fahrenheit

--- test/test_temperatura.js
+++ test/test_temperatura.js
@@ -3,22 +3,17 @@ var expect = chai.expect;
 describe("Pruebas de Temperatura y herencia",function(){
     describe("Probando herencia",function(){
       it ("Temperatura hereda de Medida",function(){
-         function Temperatura(valor,tipo){
-             Medida.call(this, valor, tipo);
-         }
-        var t1=new Medida(2,"f");
-        t1.prototype instanceof Medida;
-        t1 instanceof Temperatura;
-        //t1.prototype instanceof Medida;
+          //console.log(Medida.measures.temperatura);
+          expect(Medida.measures.temperatura.prototype).to.be.instanceof(Medida);
       });
-      it ("Celsius hereda de Temperatura.",function(){
-        function Celsius(valor) {
-            Temperatura.call(this, valor, "C");
-            Celsius.prototype = new Temperatura();
-            Celsius.prototype.constructor = Celsius;
-        }
-        
-        //Celsius.prototype instanceof Temperatura;
+      it("Celsius hereda de Medida",function(){
+        expect(Medida.measures.c.prototype).to.be.instanceof(Medida);
+      });
+      it("Kelvin hereda de Medida",function(){
+        expect(Medida.measures.k.prototype).to.be.instanceof(Medida);
+      });
+      it("Fahrenheit hereda de Medida",function(){
+        expect(Medida.measures.f.prototype).to.be.instanceof(Medida);
       });
     });
 });
