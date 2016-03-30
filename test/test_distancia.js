4951898a5834c683a3284199f5243bc27cea34ae
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-03-30 09:47:15 +0100

    Arreglando fichero de distancia

--- test/test_distancia.js
+++ test/test_distancia.js
@@ -1,25 +1,51 @@
+/*jslint browser: true, this: true*/
+/*global
+    chai describe it beforeEach afterEach sandbox sinon window console Medida
+*/
+
 var expect = chai.expect;
 
-describe("Pruebas de Distancia y herencia",function(){
-    describe("Probando herencia",function(){
-      it ("Distancia hereda de Medida",function(){
-          expect(Medida.measures.distancia.prototype).to.be.instanceof(Medida);
-      });
-      it ("Metro hereda de Distancia.",function(){
-         expect(Medida.measures.m.prototype).to.be.instanceof(Medida.measures.distancia);        
-      });
-      it ("Pulgada hereda de Distancia.",function(){
-         expect(Medida.measures.p.prototype).to.be.instanceof(Medida.measures.distancia);        
-      });
+describe("Pruebas de Distancia y herencia", function () {
+    "use strict";
+    var sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(window.console, "log");
+        sandbox.stub(window.console, "error");
     });
-    describe("Probando medidas",function(){
-    	metro = new Medida.measures.m(2);
-    	it("Se debe poder convertir a pulgadas", function(){
-    		expect(metro.toPulgada().valor).to.equal(new Medida.measures.p(78.7402).valor);
-    	});
-    	pulgada = new Medida.measures.p(15);
-    	it("Se debe poder convertir a metros", function(){
-    		expect(pulgada.toMetro().valor).to.equal(new Medida.measures.m(0.381).valor);
-    	});
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe("Distancia", function () {
+        it("Distancia hereda de Medida", function () {
+            expect(Medida.measures.distancia.prototype).to.be.instanceof(Medida);
+            sinon.assert.notCalled(console.log);
+        });
+    });
+    describe("Metro", function () {
+        it("Metro hereda de Distancia", function () {
+            expect(Medida.measures.m.prototype).to.be.instanceof(Medida.measures.distancia);
+            sinon.assert.notCalled(console.log);
+        });
+        var metro = new Medida.measures.m(2);
+        it("#toPulgada", function () {
+            expect(metro.toPulgada().valor).to.be.within(78.7, 78.8);
+            sinon.assert.notCalled(console.log);
+        });
+    });
+    describe("Pulgada", function () {
+        it("Pulgada hereda de Distancia", function () {
+            expect(Medida.measures.p.prototype).to.be.instanceof(Medida.measures.distancia);
+            sinon.assert.notCalled(console.log);
+        });
+        var pulgada = new Medida.measures.p(15);
+        it("#toMetro", function () {
+            expect(pulgada.toMetro().valor).to.be.within(0.38, 0.39);
+            sinon.assert.notCalled(console.log);
+        });
     });
 });
+
