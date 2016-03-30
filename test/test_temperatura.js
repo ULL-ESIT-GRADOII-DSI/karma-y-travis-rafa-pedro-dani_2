8350279e9728473a11df898e4df9ad6c7494e55e
alu0100505078 <alu0100505078@ull.edu.es>
2016-03-30 10:21:50 +0200

    Arreglado test temperatura con intervalos

--- test/test_temperatura.js
+++ test/test_temperatura.js
@@ -1,19 +1,34 @@
 var expect = chai.expect;
 
 describe("Pruebas de Temperatura y herencia",function(){
+    "use strict";
+    var sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(window.console, "log");
+        sandbox.stub(window.console, "error");
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
     describe("Probando herencia",function(){
       it ("Temperatura hereda de Medida",function(){
-          //console.log(Medida.measures.temperatura);
           expect(Medida.measures.temperatura.prototype).to.be.instanceof(Medida);
+          sinon.assert.notCalled(console.log);
       });
       it("Celsius hereda de Temperatura",function(){
         expect(Medida.measures.c.prototype).to.be.instanceof(Medida.measures.temperatura);
+        sinon.assert.notCalled(console.log);
       });
       it("Kelvin hereda de Temperatura",function(){
         expect(Medida.measures.k.prototype).to.be.instanceof(Medida.measures.temperatura);
+        sinon.assert.notCalled(console.log);
       });
       it("Fahrenheit hereda de Temperatura",function(){
         expect(Medida.measures.f.prototype).to.be.instanceof(Medida.measures.temperatura);
+        sinon.assert.notCalled(console.log);
       });
     });
     describe("Probando metodos Celsius",function(){
@@ -21,13 +36,16 @@ describe("Pruebas de Temperatura y herencia",function(){
         var celsius = new Medida.measures.c(2);
 
         it ("Se debe poder convertir a Fahrenheit",function(){
-          expect(celsius.toFahrenheit().valor).to.equal(new Medida.measures.f(35.6).valor);
+          expect(celsius.toFahrenheit().valor).to.be.within(35.6,35.7);
+          sinon.assert.notCalled(console.log);
         });
         it ("Se debe poder convertir a Kelvin",function(){
-          expect(celsius.toKelvin().valor).to.equal(new Medida.measures.k(275.15).valor);
+          expect(celsius.toKelvin().valor).to.be.within(275.15,275.16);
+          sinon.assert.notCalled(console.log);
         });
         it("#toString",function(){
           expect(celsius.toString()).to.equal("2.00 Celsius");
+          sinon.assert.notCalled(console.log);
         });
       });
     });
@@ -36,13 +54,16 @@ describe("Pruebas de Temperatura y herencia",function(){
         var fahrenheit = new Medida.measures.f(2);
 
         it ("Se debe poder convertir a Celsius",function(){
-          expect(fahrenheit.toCelsius().valor).to.equal(new Medida.measures.c(-16.666666666666668).valor);
+          expect(fahrenheit.toCelsius().valor).to.be.within(-16.7,-16.6);
+          sinon.assert.notCalled(console.log);
         });
         it ("Se debe poder convertir a Kelvin",function(){
-          expect(fahrenheit.toKelvin().valor).to.equal(new Medida.measures.k(256.48333333333335).valor);
+          expect(fahrenheit.toKelvin().valor).to.be.within(256.4,256.5);
+          sinon.assert.notCalled(console.log);
         });
         it("#toString",function(){
           expect(fahrenheit.toString()).to.equal("2.00 Fahrenheit");
+          sinon.assert.notCalled(console.log);
         });
       });
     });
@@ -50,13 +71,16 @@ describe("Pruebas de Temperatura y herencia",function(){
       describe("Probando Kelvin",function(){
         var kelvin = new Medida.measures.k(2);
         it ("Se debe poder convertir a Celsius",function(){
-          expect(kelvin.toCelsius().valor).to.equal(new Medida.measures.c(-271.15).valor);
+          expect(kelvin.toCelsius().valor).to.be.within(-271.16,-271.15);
+          sinon.assert.notCalled(console.log);
         });
         it ("Se debe poder convertir a Fahrenheit",function(){
-          expect(kelvin.toFahrenheit().valor).to.equal(new Medida.measures.f(-456.07).valor);
+          expect(kelvin.toFahrenheit().valor).to.be.within(-456.1,-456.0);
+          sinon.assert.notCalled(console.log);
         });
         it("#toString",function(){
           expect(kelvin.toString()).to.equal("2.00 Kelvin");
+          sinon.assert.notCalled(console.log);
         });
       });
     });
